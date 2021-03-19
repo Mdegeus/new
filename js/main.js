98e3b68ef8383aee9a163af70dbcd6d765373540
mdegeus <302639117@student.rocmondriaan.nl>
2021-03-19 15:58:47 +0100

    footer and back to top button

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const btnToTop =document.querySelector(".btn-back-top");
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+btnToTop.addEventListener('click', topFunction);
