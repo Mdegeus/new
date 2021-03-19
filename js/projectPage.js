4e941926b31c8cbb338e48d754cbf9a56a56daf3
mdegeus <302639117@student.rocmondriaan.nl>
2021-03-19 17:19:24 +0100

    small issue with path of project button fixed

--- js/projectPage.js
+++ js/projectPage.js
@@ -3,6 +3,6 @@ const projectsBtn = document.querySelectorAll(".btn-project");
 for (let i = 0; i < projectsBtn.length; i++) {
     console.log('btn');
     projectsBtn[i].addEventListener('click', function(e){
-        let page = window.open("../projectHtmls/"+projectsBtn[i].id+'.html');
+        let page = window.open("https://mdegeus.github.io/new/projectHtmls/"+projectsBtn[i].id+'.html');
     });
 }
