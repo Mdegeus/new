35c4571d05fd0d0437ccfb56179bd3456efbdd1e
mdegeus <302639117@student.rocmondriaan.nl>
2021-03-22 12:36:13 +0100

    New theme button switcher, theme css added

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ function topFunction() {
 }
 
 btnToTop.addEventListener('click', topFunction);
+
+if (localStorage.getItem('Theme') == null || localStorage.getItem('Theme') == ""){
+    localStorage.setItem('Theme', 'dark-theme');
+}
+
+if (document.querySelector('.theme-switcher')){
+    const button = document.querySelector('.theme-switcher')
+    button.addEventListener('click', function(){
+        if (localStorage.getItem('Theme') === "dark-theme"){
+            localStorage.setItem('Theme', "light-theme");
+            document.documentElement.className = localStorage.getItem('Theme')
+            button.textContent = "<Theme: Light>"
+
+        }else{
+            localStorage.setItem('Theme', "dark-theme");
+            document.documentElement.className = localStorage.getItem('Theme')
+            button.textContent = "<Theme: Dark>"
+        }
+    });
+}
+
+
+document.documentElement.className = localStorage.getItem('Theme')
