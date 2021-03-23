fd33abc641069924be8de44a1fc6a6b062e6c027
mdegeus <302639117@student.rocmondriaan.nl>
2021-03-23 13:51:39 +0100

    new button that lets you choose if you want to switch the theme by time or not, time based theme switcher: between 10pm and 6am is dark rest is light

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const btnToTop = document.querySelector(".btn-back-top");
+const btnTB = document.querySelector(".btn-timeBased");
+const button = document.querySelector('.theme-switcher');
 
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
@@ -11,28 +13,60 @@ if (localStorage.getItem('Theme') == null || localStorage.getItem('Theme') == ""
     localStorage.setItem('Theme', 'dark-theme');
 }
 
-if (document.querySelector('.theme-switcher')){
-    const button = document.querySelector('.theme-switcher')
-
-    if (localStorage.getItem('Theme') === "dark-theme"){
-        button.textContent = "<Theme: Dark>"
-    }else{
-        button.textContent = "<Theme: Light>"
-    }
-
-    button.addEventListener('click', function(){
-        if (localStorage.getItem('Theme') === "dark-theme"){
-            localStorage.setItem('Theme', "light-theme");
-            document.documentElement.className = localStorage.getItem('Theme')
-            button.textContent = "<Theme: Light>"
-
-        }else{
-            localStorage.setItem('Theme', "dark-theme");
-            document.documentElement.className = localStorage.getItem('Theme')
-            button.textContent = "<Theme: Dark>"
-        }
-    });
+if (localStorage.getItem('Theme') == null || localStorage.getItem('Theme') == ""){
+    localStorage.setItem('Theme', 'dark-theme');
 }
 
+if (localStorage.getItem('Theme') === "dark-theme"){
+    button.textContent = "<Theme: Dark>";
+}else{
+    button.textContent = "<Theme: Light>";
+}
+
+button.addEventListener('click', function(){
+    if (localStorage.getItem('Theme') === "dark-theme"){
+        localStorage.setItem('Theme', "light-theme");
+        document.documentElement.className = localStorage.getItem('Theme');
+        button.textContent = "<Theme: Light>";
+
+    }else{
+        localStorage.setItem('Theme', "dark-theme");
+        document.documentElement.className = localStorage.getItem('Theme');
+        button.textContent = "<Theme: Dark>";
+    }
+});
 
 document.documentElement.className = localStorage.getItem('Theme')
+
+
+function SwitchWithTime(){
+    if (localStorage.getItem('timeBased-Theme') == 'true'){
+        const date = new Date();
+        if (date.getHours() > 22 && date.getHours() < 6){
+            localStorage.setItem('Theme', "dark-theme");
+            document.documentElement.className = localStorage.getItem('Theme');
+            button.textContent = "<Theme: Dark>";
+        }else{
+            localStorage.setItem('Theme', "light-theme");
+            document.documentElement.className = localStorage.getItem('Theme');
+            button.textContent = "<Theme: Light>";
+        }
+    }
+}
+
+function timeBasedbtnClick(){
+        btnTB.textContent = "timeBased: " + localStorage.getItem('timeBased-Theme');
+}
+
+btnTB.addEventListener('click', function(){
+    if (localStorage.getItem('timeBased-Theme') == 'true'){
+        localStorage.setItem('timeBased-Theme', false)
+    }else{
+        localStorage.setItem('timeBased-Theme', true)
+        SwitchWithTime();
+    }
+    timeBasedbtnClick()
+});
+
+setInterval(SwitchWithTime ,1000)
+timeBasedbtnClick()
