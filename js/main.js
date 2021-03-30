83eee6fc33c510513e1d57c93b4b3ed7f0f4a0b5
mdegeus <302639117@student.rocmondriaan.nl>
2021-03-30 20:15:23 +0200

    Alt key function on homebutton removed along with all its other code that had to do with that function

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const btnToTop = document.querySelector(".btn-back-top");
 const btnTB = document.querySelector(".btn-timeBased");
 const button = document.querySelector('.theme-switcher');
 
-const bgURL = 'https://www.youtube.com/embed/gdZLi9oWNZg'
-
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
@@ -11,11 +9,11 @@ function topFunction() {
 
 btnToTop.addEventListener('click', topFunction);
 
-if (localStorage.getItem('Theme') == null || localStorage.getItem('Theme') == ""){
+if (localStorage.getItem('Theme') === null || localStorage.getItem('Theme') === ""){
     localStorage.setItem('Theme', 'dark-theme');
 }
 
-if (localStorage.getItem('Theme') == null || localStorage.getItem('Theme') == ""){
+if (localStorage.getItem('Theme') === null || localStorage.getItem('Theme') === ""){
     localStorage.setItem('Theme', 'dark-theme');
 }
 
@@ -43,7 +41,7 @@ document.documentElement.className = localStorage.getItem('Theme')
 
 
 function SwitchWithTime(){
-    if (localStorage.getItem('timeBased-Theme') == 'true'){
+    if (localStorage.getItem('timeBased-Theme') === 'true'){
         const date = new Date();
         if (date.getHours() > 22 && date.getHours() < 6){
             localStorage.setItem('Theme', "dark-theme");
@@ -62,7 +60,7 @@ function timeBasedbtnClick(){
 }
 
 btnTB.addEventListener('click', function(){
-    if (localStorage.getItem('timeBased-Theme') == 'true'){
+    if (localStorage.getItem('timeBased-Theme') === 'true'){
         localStorage.setItem('timeBased-Theme', false)
     }else{
         localStorage.setItem('timeBased-Theme', true)
@@ -74,26 +72,6 @@ btnTB.addEventListener('click', function(){
 setInterval(SwitchWithTime ,1000)
 timeBasedbtnClick()
 
-function GlitchState(){
-    const all = document.querySelectorAll("*")
-    for (let i = 0; i < all.length; i++){
-        const taggsName = all[i].tagName.toLowerCase();
-        if (taggsName != 'html' && taggsName != 'html' && taggsName != 'body'){
-            all[i].style.visibility = 'hidden';
-        }
-    }
-}
-
 document.querySelector('.HomeButton').addEventListener('click', function(e){
-    if (e.altKey){
-        e.preventDefault();
-        const audio = new Audio('../audio/kh.mp3');
-        audio.play();
-        //document.style.animation = change-background;
-        GlitchState();
-        document.write(`<p>HI</p>`);
-        setInterval(function(){
-            document.write(`<p>HI</p>`);
-        }, 1000)
-    }
+    e.preventDefault();
 });
