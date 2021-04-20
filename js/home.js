4e9e2219670d89e1b4917a1e981a11ef3f1f2b0f
mdegeus <302639117@student.rocmondriaan.nl>
2021-04-20 23:35:30 +0200

    cards of project and profession profile work, profession profile page added. new button color variables

---
+++ js/home.js
@@ -0,0 +1,10 @@
+const projectBtn = document.querySelector('.project-btn')
+const professionProfileBtn = document.querySelector('.professionProfile-btn')
+
+projectBtn.addEventListener('click', function(){
+    document.location = '../projects.html'
+});
+
+professionProfileBtn.addEventListener('click', function(){
+    document.location = '../professionProfile.html'
+});
