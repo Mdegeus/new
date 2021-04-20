307f8bc2b8706022157cb89641e2fff11cbbc204
mdegeus <302639117@student.rocmondriaan.nl>
2021-04-20 23:38:22 +0200

    path updated

--- js/home.js
+++ js/home.js
@@ -2,9 +2,9 @@ const projectBtn = document.querySelector('.project-btn')
 const professionProfileBtn = document.querySelector('.professionProfile-btn')
 
 projectBtn.addEventListener('click', function(){
-    document.location = '../projects.html'
+    document.location = 'https://mdegeus.github.io/new/projects.html'
 });
 
 professionProfileBtn.addEventListener('click', function(){
-    document.location = '../professionProfile.html'
+    document.location = 'https://mdegeus.github.io/new/professionProfile.html'
 });
