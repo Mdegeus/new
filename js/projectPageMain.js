0162f85b908fc2d21fc31d6f5844e70bb2e318d0
mdegeus <302639117@student.rocmondriaan.nl>
2021-06-27 16:35:58 +0200

    code changed

--- js/projectPageMain.js
+++ js/projectPageMain.js
@@ -1,3 +1,15 @@
-document.querySelector('.closePage').addEventListener('click', function() {
-    window.close();
-});
+if (document.querySelector('.closePage')) {
+    
+    document.querySelector('.closePage').addEventListener('click', function() {
+        window.close();
+    });
+}
+
+if (document.querySelector('.projectButton')) {
+
+    document.querySelector('.projectButton').addEventListener('click', function() {
+        window.open(document.querySelector('.projectButton').id);
+    });
+
+}
+
